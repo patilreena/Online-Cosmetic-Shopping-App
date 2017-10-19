ae0c136eec4a745e152ed7614488f8eb0562e8ab
kasumi <kasumi0201@gmail.com>
2017-10-19 16:39:46 +1100

    set routes to review

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :profiles
+  resources :reviews
 
   devise_for :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019032534) do
+ActiveRecord::Schema.define(version: 20171019052113) do
 
   create_table "profiles", force: :cascade do |t|
     t.text "avatar_data"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20171019032534) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
